a5abf9779afc358bc58388fba5acfebd7120cc77
Fabio <fabiocerqueiracw@gmail.com>
2016-07-06 00:02:28 +0200

    Basic main templating

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title') - Carnet</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
-
+    <div class="mdl-layout mdl-js-layout">
+        <header class="mdl-layout__header"></header>
+        <div class="mdl-layout__content">
+            @yield('content')
+        </div>
+        <footer class="mdl-layout__footer">
+            @yield('footer')
+        </footer>
+    </div>
     @if(Session::has('error'))
            {{ Toastr::error(Session::get('error')) }}
     @endif
