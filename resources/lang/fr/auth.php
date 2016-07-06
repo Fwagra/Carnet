1ed6805338f596b8b77ccad23ff3cc434a11a8c2
Fabio <fabiocerqueiracw@gmail.com>
2016-07-06 23:56:34 +0200

    Register page

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ces identifiants n\'existent pas.',
+    'throttle' => 'Trop de tentatives de connexion. Réessayez dans :seconds secondes.',
+    'register_title' => 'Inscription',
+    'name_form' => 'Identifiant',
+    'email_form' => 'Email',
+    'name_form' => 'Identifiant',
+    'password_form' => 'Mot de passe',
+    'confirm_password_form' => 'Retapez votre mot de passe',
+    'register_btn' => "S'inscrire"
+];
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+@section('title', trans('auth.register_title'))
+@section('content')
+<div class="content-grid mdl-grid">
+    <div class="mdl-layout-spacer"></div>
+    <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet  mdl-cell--12-col-tablet">
+        <div class="mdl-card mdl-shadow--4dp">
+            <div class="mdl-card__title">
+                <h1 class="mdl-card__title-text">{!! trans('auth.register_title') !!}</h1>
+            </div>
+            <div class="mdl-card__supporting-text">
+                <form class="" role="form" method="POST" action="{{ url('/register') }}">
+                    {{ csrf_field() }}
+
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label {{ $errors->has('name') ? ' has-error' : '' }}">
+                        <input id="name" type="text" class="mdl-textfield__input" name="name" value="{{ old('name') }}">
+                        <label for="name" class="mdl-textfield__label">{!! trans('auth.name_form') !!}</label>
+                            @if ($errors->has('name'))
+                                <span class="mdl-textfield__error">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </span>
+                            @endif
+                    </div>
+
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <input id="email" type="email" class="mdl-textfield__input" name="email" value="{{ old('email') }}">
+                            <label for="email" class="mdl-textfield__label">{!! trans('auth.email_form') !!}</label>
+                            @if ($errors->has('email'))
+                                <span class="mdl-textfield__error">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                    </div>
+
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <input id="password" type="password" class="mdl-textfield__input" name="password">
+                        <label for="password" class="mdl-textfield__label">{!! trans('auth.password_form') !!}</label>
+
+                            @if ($errors->has('password'))
+                                <span class="mdl-textfield__error">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                    </div>
+
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                        <input id="password-confirm" type="password" class="mdl-textfield__input" name="password_confirmation">
+                        <label for="password-confirm" class="mdl-textfield__label">{!! trans('auth.confirm_password_form') !!}</label>
+                            @if ($errors->has('password_confirmation'))
+                                <span class="mdl-textfield__error">
+                                    <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                </span>
+                            @endif
+                    </div>
+
+                    <div class="mdl-card__actions">
+                        <button type="submit" class="mdl-button mdl-js-button mdl-button--colored mdl-button--raised mdl-js-ripple-effect">
+                            <i class="fa fa-btn fa-user"></i> {!! trans('auth.register_btn') !!}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="mdl-layout-spacer"></div>
+</div>
+@endsection
