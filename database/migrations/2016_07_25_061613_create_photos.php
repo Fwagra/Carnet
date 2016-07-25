f6b3407728f5e837d3883fb33a33deeaa1c1c382
Fabio <fabiocerqueiracw@gmail.com>
2016-07-25 08:17:58 +0200

    Photos - Model, controller, migrations

---
+++ database/migrations/2016_07_25_061613_create_photos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('name');
+            $table->string('filename');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('photos');
+    }
+}
