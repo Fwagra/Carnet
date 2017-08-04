7b786f7a2736316d87463e9e094e7f7ddba08259
Fabio <fabiocerqueiracw@gmail.com>
2017-08-04 14:33:50 +0200

    Photo -  Delete relation to steps on deletion

--- app/Http/Observers/PhotoObserver.php
+++ app/Http/Observers/PhotoObserver.php
@@ -1,19 +1,29 @@
 <?php namespace App\Http\Observers;
 
 use App\Photo;
+use App\Step;
 use App\Trip;
 
 class PhotoObserver {
 
   /**
-   * Detach the head image of a trip when the image is deleted
+   * Detach the head image of a trip or step when the image is deleted
    * @param \App\Photo $model
    */
   public function deleted($model){
     $trips = Trip::where('image_id', $model->id)->get();
-    foreach ($trips as $trip) {
-      $trip->image_id = 0;
-      $trip->save();
+    $steps = Step::where('image_id', $model->id)->get();
+    if (count($trips)){
+      foreach ($trips as $trip) {
+        $trip->image_id = 0;
+        $trip->save();
+      }
+    }
+    if (count($steps)) {
+      foreach ($steps as $step) {
+        $step->image_id = 0;
+        $step->save();
+      }
     }
   }
 
